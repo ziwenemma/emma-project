a428a93e9a88ca62863abb0d2557b694134c99c0
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-02 09:39:07 +0800

    change spelling

--- components/IntroPart.js
+++ components/IntroPart.js
@@ -5,7 +5,7 @@ function IntroPart() {
       <div className='w-full h-80 bg-back-img bg-no-repeat  bg-center border'>
       <p className='text-5xl md:text-6xl  pt-20 text-center font-bold'>Movie Museum</p>
       <div className='flex items-center justify-center pt-10'>
-      <button className='object-center rounded-lg px-5 py-2 bg-yellow-400 text-white hover:bg-yellow-600 hover:text-blue-100 duration-200'>Let's Start</button></div>
+      <button className='object-center rounded-lg px-5 py-2 bg-yellow-400 text-white hover:bg-yellow-600 hover:text-blue-100 duration-200'>{"Let's Start"}</button></div>
        </div>
       </div>
     </div>
