ac8b9b5c6337144160576eef62021498b1498d9d
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-02 09:32:26 +0800

    wrong classname

--- components/Footer.js
+++ components/Footer.js
@@ -28,23 +28,23 @@ function Footer() {
       <div className="px-4">
         <h5 className="text-2xl text-white font-bold mb-6">Help</h5>
         <ul className="list-none footer-links">
-          <li class="mb-2">
+          <li className="mb-2">
             <a href="#" className="transparent hover:text-purple-400">Support</a>
           </li>         
         </ul>
       </div>
         </div>
 
-    <div class="sm:flex sm:flex-wrap  border-t  border-white justify-evenly">
-      <div class="px-4 pt-2">
-        <h6 class="font-bold text-2xl text-white mb-2">Address</h6>
-        <address class="not-italic mb-4 text-sm">
+    <div className="sm:flex sm:flex-wrap  border-t  border-white justify-evenly">
+      <div className="px-4 pt-2">
+        <h6 className="font-bold text-2xl text-white mb-2">Address</h6>
+        <address className="not-italic mb-4 text-sm">
           ChengDu
         </address>
       </div>
-      <div class="px-4 pt-2">
-        <h6 class="font-bold text-2xl text-white mb-2">Email</h6>
-        <p class="mb-4 text-sm">973051062@qq.com</p>
+      <div className="px-4 pt-2">
+        <h6 className="font-bold text-2xl text-white mb-2">Email</h6>
+        <p className="mb-4 text-sm">973051062@qq.com</p>
       </div>
     </div>
   </div>
