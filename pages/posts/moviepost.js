fa54adf620da7c30dac74c5583cee3450fe9fc4f
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-03 15:24:11 +0800

    将json里面的内容使用getstaticprops导出

--- pages/posts/moviepost.js
+++ pages/posts/moviepost.js
@@ -5,6 +5,7 @@ import Footer from '/components/Footer.js';
 import Header from '/components/Header.js';
 
 
+
 export function MoviePage({playlistLists}){
   return (
     <div> 
@@ -20,15 +21,15 @@ export function MoviePage({playlistLists}){
          {playlistLists.map(playlistLists =>(
            <div className='rounded-lg overflow-hidden shadow-lg justify-center items-center bg-white bg-opacity-90 p-10'>
           <div className='w-full'>
-             <img src={playlistLists.image} alt=''width='300px' height='200px'/></div>
+             <img src={playlistLists.image} alt=''width='300' height='200'/></div>
           <div className='w-full p-4 justify-start flex flex-col'>
            <h2 className='border-b-2 text-black font-bold text-3xl'>{playlistLists.title}</h2>
            <p className='text-black my-3 leading-normal text-sm'>{playlistLists.desc}</p>
            <p className='text-[#1E90FF] text-2xl'>{playlistLists.rating}</p>
           </div>
-        <div class="pt-4 pb-2">
-        <span class="inline-block bg-blue-500 rounded-full  py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{playlistLists.info.date}</span>
-        <span class="inline-block bg-pruple-400 bg-opacity-60 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{playlistLists.info.category}</span>
+        <div className="pt-4 pb-2">
+        <span className="inline-block bg-blue-500 rounded-full  py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">{playlistLists.info.date}</span>
+        <span className="inline-block bg-pruple-400 bg-opacity-60 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{playlistLists.info.category}</span>
       </div>
            </div>
          ))}
