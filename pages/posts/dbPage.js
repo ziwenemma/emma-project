fb3d8ace71c630b60597a21a134a552aa8bcd244
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-05 14:34:19 +0800

    修改code使连接后端mongodb，从而可以进行查询列表/查询详情/新增/删除/修改

--- pages/posts/dbPage.js
+++ pages/posts/dbPage.js
@@ -1,7 +1,23 @@
-const {MongoClient}=require("mongodb")
+require('dotenv').config();
 
-client.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
+const mongoose=require("mongoose");
+const mongodb = require("mongodb");
+const { Schema }=mongoose;
+
+const uri = 'mongodb+srv://emmama:973051062@cluster0.aeqwf.mongodb.net/movie_db?retryWrites=true&w=majority';
+
+mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true,})
 .then(() => console.log('DB Connected!'))
 .catch(err => {
 console.log(Error, err.message);
 });
+
+const movieSchema=new mongoose.Schema({title: {type:String, required:true},img:String,desc:String,date:{type:Date,default:Date.now}},{collection:"Movie"});
+const Movie = mongoose.model('Movie',movieSchema);
+
+
+
+exports.MovieModel = Movie;
+
+
+
