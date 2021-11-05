a69cbffb79415b0da148ef785a8ad7d917f9358f
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-05 10:46:47 +0800

    尝试将页面连接到mongodb

--- pages/posts/dbPage.js
+++ pages/posts/dbPage.js
@@ -1,11 +1,6 @@
-require('dotenv').config();
+const {MongoClient}=require("mongodb")
 
-const mongoose=require("mongoose");
-const mongodb = require("mongodb");
-
-const uri = 'mongodb+srv://emmama:973051062@cluster0.aeqwf.mongodb.net/movie_list?retryWrites=true&w=majority';
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,})
+client.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
 .then(() => console.log('DB Connected!'))
 .catch(err => {
 console.log(Error, err.message);
