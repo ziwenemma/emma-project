5ea695e89d891d8bc93dc244d00c6a9ca3752cf3
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-19 19:00:48 +0800

    第一次样式修改

--- pages/newsPage.js
+++ pages/newsPage.js
@@ -13,19 +13,30 @@ export default function NewsPage({ properties }) {
       <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      
+
     <div>
       <h1 className='text-5xl text-black text-center p-10'>News</h1>
     </div>    
-     <div className='flex flex-row flex-wrap'>
+    <div className='m-4 p-10 py-6 lg:py-2'>
+     <div className='grid grid-cols-1 gap-2 relative w-8 pt-10'>
        {properties&&properties.map(property=>(
-        <div className='card mb-2'>
-        <div className="card-body">
-          <div className='h5 text-uppercase'>{property.title}</div>
-          <p className='fw-light'>{property.desc}</p>
+        <div className="md:flex shadow-lg  mx-6 md:mx-auto my-40 max-w-lg md:max-w-2xl h-64">
+        <img className="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6" src={property.img} alt=""/>
+        <div className="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
+           <div className="flex items-center">
+              <h2 className="text-xl text-gray-800 font-medium mr-auto">{property.title}</h2>
+              <p className="text-gray-800 font-semibold tracking-tighter">
+              {property.data}
+              </p>
+           </div>
+           <p className="text-sm text-gray-700 mt-4">
+              {property.desc}
+           </p>
         </div>
-        </div>
-       ))}
+     </div>
+   
+       ))} 
+     </div>
      </div>
      </div>
   )
