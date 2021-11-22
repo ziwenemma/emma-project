f5821950b4b19cc0379ddf39bab5beabca67367e
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-22 10:54:58 +0800

    修改json

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   purge: ['./pages/**/*,{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    textColor: theme => theme('colors'),
-    textColor: {
-      'primary': '#3490dc',
-      'secondary': '#ffed4a',
-      'danger': '#e3342f',
-    },
     extend: {
       backgroundImage:(theme)=>({
         'back-img':'url("/bg.png")',
