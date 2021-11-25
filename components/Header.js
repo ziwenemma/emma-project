862111e7299784a0d58e0fd7ba0ee03d3cdfdbf4
ZiWen Ma <ziwenma@ZiWendeMacBook.local>
2021-11-25 16:11:57 +0800

    feat:不使用herioicons

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,50 @@
-import {
-  CollectionIcon, FilmIcon, HomeIcon,
-  PhotographIcon
-} from "@heroicons/react/outline";
+import { AiOutlineHome, AiOutlineInstagram } from "react-icons/ai";
+import { BsFilm, BsNewspaper } from "react-icons/bs";
 import Image from "next/image";
 import Link from "next/link";
 import HeaderItem from "./HeaderItem";
 
-
-
 function Header() {
   return (
-    <header className='flex flex-col sm:flex-row m-0 justify-between items-center'>  
-    <Image
-      className="object-fill justify-start" 
-      src="/logo.png" 
-      width={100} height={80}/>
+    <header className="flex flex-col sm:flex-row m-0 justify-between items-center">
+      <Image
+        className="object-fill justify-start"
+        src="/logo.png"
+        width={100}
+        height={80}
+      />
 
       <div className="flex flex-grow  text-white justify-start max-w-3xl  pt-10">
-      <Link href="/"><a><HeaderItem title="HOME" Icon={HomeIcon}/></a></Link>
-        <Link href="/posts/moviepost"><a><HeaderItem title="
-        MOVIES" Icon={PhotographIcon}
-        /></a></Link>
-        <Link href="/newsPage"><a><HeaderItem title="NEWS" Icon={CollectionIcon}/></a></Link>
+        <Link href="/">
+          <a>
+            <HeaderItem title="HOME" Icon={AiOutlineHome} />
+          </a>
+        </Link>
+        <Link href="/posts/moviepost">
+          <a>
+            <HeaderItem
+              title="
+        MOVIES"
+              Icon={BsFilm}
+            />
+          </a>
+        </Link>
+        <Link href="/newsPage">
+          <a>
+            <HeaderItem title="NEWS" Icon={BsNewspaper} />
+          </a>
+        </Link>
       </div>
 
       <div className="flex flex-grow text-white justify-end pt-10">
-      <Link href="/newsPage"><a><HeaderItem title="Vedio" Icon={FilmIcon}/></a></Link>
+        <Link href="/newsPage">
+          <a>
+            <HeaderItem title="Vedio" Icon={AiOutlineInstagram} />
+          </a>
+        </Link>
       </div>
-    
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
